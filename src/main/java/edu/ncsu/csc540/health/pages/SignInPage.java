e9b8955b4a89d192490d905faf36c61d324405ad
Jamie Gachie <jrgachie@ncsu.edu>
2019-11-02 19:54:37 -0400

    Fully fleshed out SignInPage (as much as possible without database interfaces).

--- src/main/java/edu/ncsu/csc540/health/pages/SignInPage.java
+++ src/main/java/edu/ncsu/csc540/health/pages/SignInPage.java
@@ -3,22 +3,62 @@ package edu.ncsu.csc540.health.pages;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextTerminal;
 
+import java.sql.Date;
+import java.util.function.Consumer;
+
 /**
  * The Sign-In page is the area where a user enters their information in
  * order to sign in to their user account. This page will display a series
  * of prompts, after each of which the user will input the corresponding
  * information.
  */
-public class SignInPage {
+public class SignInPage implements Consumer<TextIO> {
 
     @Override
     public void accept(TextIO textIO) {
         TextTerminal<?> terminal = textIO.getTextTerminal();
         terminal.println("Sign In");
         terminal.println("=====================");
-        /*
-        TODO: List all facilities here (will do after database interfaces have been implemented)
-         */
-        terminal.println("A. Please enter the number of your facility: ");
+        terminal.println("1. Sign In");
+        terminal.println("2. Go Back\n");
+
+        int option = textIO.newIntInputReader()
+                .withMinVal(1)
+                .withMaxVal(2)
+                .read("> ");
+
+        switch (option) {
+            case 1:
+                /*
+                TODO: List all facilities here (will do after database interfaces have been implemented)
+                 */
+                terminal.println("A. Please enter your Facility ID:\n");
+                int facilityID = textIO.newIntInputReader().withMinVal(1).read("> "); //TODO: Add withMaxVal() after database interface implementation
+
+                terminal.println("\nB. Please enter your last name:\n");
+                String lastName = textIO.newStringInputReader().read("> ");
+
+                terminal.println("\nC. Please enter your date of birth (mm/dd/yyyy):\n");
+                String[] dateArray = textIO.newStringInputReader().read("> ").split("/");
+                Date date = new Date(Integer.parseInt(dateArray[0]),
+                        Integer.parseInt(dateArray[1]),
+                        Integer.parseInt(dateArray[2]));
+
+                terminal.println("\nD. Please enter the city listed on your home address:\n");
+                String city = textIO.newStringInputReader().read("> ");
+
+                terminal.println("\nE. Are you a patient? (y/n)\n");
+                String patient = textIO.newStringInputReader().withPossibleValues("y", "n", "yes", "no").read("> ");
+
+                /*
+                TODO: Query database for tuple matching given information to validate user
+                 */
+
+                break;
+            case 2:
+                //TODO: Add a link back to the home page
+                terminal.println("Coward.");
+                break;
+        }
     }
 }

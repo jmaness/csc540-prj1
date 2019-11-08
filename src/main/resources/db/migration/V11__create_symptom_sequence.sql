5280509e29e224b2858f937ef6936e213d39ae93
Jamie Gachie <jrgachie@ncsu.edu>
2019-11-07 19:53:50 -0500

    Added '0' padding to symptom sequence.

--- src/main/resources/db/migration/V11__create_symptom_sequence.sql
+++ src/main/resources/db/migration/V11__create_symptom_sequence.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE TRIGGER symptoms_on_insert
     BEFORE INSERT ON symptoms
     FOR EACH ROW
 BEGIN
-    SELECT CONCAT('SYM', symptom_sequence.nextval)
+    SELECT CONCAT('SYM' || LPAD(symptom_sequence.nextval, 3, '0'))
     INTO :new.code
     FROM dual;
 END;

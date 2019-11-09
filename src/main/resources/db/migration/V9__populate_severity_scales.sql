7de3917ff12a5fa8836c5dde318c79f800266c48
jcsun <jcsun@ncsu.edu>
2019-11-09 14:27:10 -0500

    Update V9__populate_severity_scales.sql
    update with sample data values

--- src/main/resources/db/migration/V9__populate_severity_scales.sql
+++ src/main/resources/db/migration/V9__populate_severity_scales.sql
@@ -10,10 +10,36 @@ insert into severity_scale_values (id, severity_scale_id, name, ordinal) values 
 insert into severity_scale_values (id, severity_scale_id, name, ordinal) values (9, 1, '9', 9);
 insert into severity_scale_values (id, severity_scale_id, name, ordinal) values (10, 1, '10', 10);
 
+insert into severity_scales(id, name) values (2, 'Normal/Severe');
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (11, 2, 'Normal', 1);
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (12, 2, 'Severe', 2);
+
+insert into severity_scales(id, name) values (id, 3, 'Low/High');
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (13, 3, 'Low', 1);
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (14, 3, 'High', 2);
+
+insert into severity_scales(id, name) values (4, 'Normal/Premium');
+ 1);
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (15, 3, 'Normal', 1);
+insert into severity_scale_values(id, severity_scale_id, name, ordinal) values (16, 3, 'Premium', 2);
+
+insert into body_parts (code, name) values ('1', 'Abdominal');
+insert into body_parts (code, name) values ('2', 'Head');
+insert into body_parts (code, name) values ('3', 'Eye');
+
+/*
 insert into body_parts (code, name) values ('1', 'Head');
 insert into body_parts (code, name) values ('2', 'Heart');
 insert into body_parts (code, name) values ('3', 'Lungs');
 insert into body_parts (code, name) values ('4', 'Eyes');
+*/
+
+insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM001', 'Pain', 1, null);
+insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM002', 'Diarrhea', 2, '1');
+insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM003', 'Fever', 3, null);
+insert into symptoms (code, name, severity_scale_id, body_part_code)values ('SYM004', 'Physical Exam', 4, null);
+insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM005', 'Lightheadedness', 2, '2');
+insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM006', 'Blurred vision', 2, '3');
 
 insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM01', 'Fever', 1, null);
 insert into symptoms (code, name, severity_scale_id, body_part_code) values ('SYM02', 'Headache', 1, '1');

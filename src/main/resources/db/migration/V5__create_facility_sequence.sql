4e7689e378250133139513d499bab011f848558e
jcsun <jcsun@ncsu.edu>
2019-11-09 16:47:05 -0500

    Update V5__create_facility_sequence.sql
    Doesn't overwrite the sample data that starts at 1000

--- src/main/resources/db/migration/V5__create_facility_sequence.sql
+++ src/main/resources/db/migration/V5__create_facility_sequence.sql
@@ -1,10 +1,10 @@
-create sequence facility_sequence;
+create sequence facility_sequence START WITH 2000;
 
 CREATE OR REPLACE TRIGGER facilities_on_insert
     BEFORE INSERT ON facilities
     FOR EACH ROW
 BEGIN
-    SELECT facility_sequence.nextval
+    SELECT COALESCE(:new.id, facility_sequence.nextval)
     INTO :new.id
     FROM dual;
 END;

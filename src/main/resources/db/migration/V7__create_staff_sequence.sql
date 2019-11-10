0b4c1bcf473192139e6b052e4852de2693d9df1d
jcsun <jcsun@ncsu.edu>
2019-11-09 22:05:16 -0500

    Update V7__create_staff_sequence.sql
    The sample staff IDs are in the 60000-90000 range

--- src/main/resources/db/migration/V7__create_staff_sequence.sql
+++ src/main/resources/db/migration/V7__create_staff_sequence.sql
@@ -1,10 +1,11 @@
-create sequence staff_sequence;
+create sequence staff_sequence START WITH 100000;;
 
 CREATE OR REPLACE TRIGGER staff_on_insert
     BEFORE INSERT ON staff
     FOR EACH ROW
 BEGIN
     SELECT staff_sequence.nextval
+	SELECT COALESCE(:new.id, staff_sequence.nextval)
     INTO :new.id
     FROM dual;
 END;

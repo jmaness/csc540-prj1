687f91be31677b5c81622219bc4bfe4f2fff75d5
jcsun <jcsun@ncsu.edu>
2019-11-11 12:37:24 -0500

    Update V6__insert_example_facilities.sql
    update facilitie addresses as well

--- src/main/resources/db/migration/V6__insert_example_facilities.sql
+++ src/main/resources/db/migration/V6__insert_example_facilities.sql
@@ -7,15 +7,15 @@ insert into services (code, name) values ('SER01', 'Emergency');
 insert into services (code, name) values ('SGP01', 'General practice');
 insert into services (code, name) values ('VIS01', 'Vision');
 
-insert into addresses (num, street, city, state, zip, country) values (2650, 'Wolf Village Way Box 7220', 'Raleigh', 'NC', 27695, 'US');
+insert into addresses (num, street, city, state, country) values (2650, 'Wolf Village Way Box 7220', 'Raleigh', 'NC', 'US');
 insert into facilities (id, name, capacity, classification_code, address_id) values (1000, 'Wolf Hospital', 300, '03', 1);
 insert into facility_certifications (facility_id, certification_acronym, certification_date, expiration_date) values (1000, 'CSC000', TO_DATE('01/01/2000', 'MM/DD/YYYY'), TO_DATE('01/01/2020', 'MM/DD/YYYY'));
 
-insert into addresses (num, street, city, state, zip, country) values (2650, '2500 Sacramento', 'Santa Cruz', 'CA', 90021, 'US');
+insert into addresses (num, street, city, state, country) values (2650, '2500 Sacramento', 'Santa Cruz', 'CA', 'US');
 insert into facilities (id, name, capacity, classification_code, address_id) values (1001, 'California Health Care', 150, '02', 2);
 insert into facility_certifications (facility_id, certification_acronym, certification_date, expiration_date) values (1001, 'ISO000', TO_DATE('01/01/2001', 'MM/DD/YYYY'), TO_DATE('01/01/2021', 'MM/DD/YYYY'));
 
-insert into addresses (num, street, city, state, zip, country) values (489, 'First Avenue', 'New York', 'NY', 10001, 'US');
+insert into addresses (num, street, city, state, country) values (489, 'First Avenue', 'New York', 'NY', 'US');
 insert into facilities (id, name, capacity, classification_code, address_id) values (1002, 'Suny Medical Center', 10, '01', 3);
 insert into facility_certifications (facility_id, certification_acronym, certification_date, expiration_date) values (1002, 'ISO001', TO_DATE('01/01/2002', 'MM/DD/YYYY'), TO_DATE('01/01/2022', 'MM/DD/YYYY'));
 insert into facility_certifications (facility_id, certification_acronym, certification_date, expiration_date) values (1002, 'PSC000', TO_DATE('01/01/2000', 'MM/DD/YYYY'), TO_DATE('01/01/2020', 'MM/DD/YYYY'));

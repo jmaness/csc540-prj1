e08b8e538455afa55c3b3635679c7c1966316d51
jcsun <jcsun@ncsu.edu>
2019-11-13 12:55:50 -0500

    tab/space now actually aligned

--- src/main/resources/db/migration/V3__create_address_sequence.sql
+++ src/main/resources/db/migration/V3__create_address_sequence.sql
@@ -2,9 +2,9 @@ create sequence address_sequence START WITH 100;
 
 CREATE OR REPLACE TRIGGER addresses_on_insert
     BEFORE INSERT ON addresses
-	FOR EACH ROW
+    FOR EACH ROW
 BEGIN
     SELECT COALESCE(:new.id, address_sequence.nextval)
-	INTO :new.id
+    INTO :new.id
     FROM dual;
 END;
